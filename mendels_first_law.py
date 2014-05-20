d17dd4dab9024d8ad05b19c6d88103cc4c27bb16
Andrew Duberstein <ajduberstein@gmail.com>
2014-05-20 10:55:03 -0400

    Create mendels_first_law.py

---
+++ mendels_first_law.py
@@ -0,0 +1,20 @@
+# Simulation of gene pool
+def mendelian(k,m,n):
+        gene_pool = []
+        for i in xrange(0,k):
+                gene_pool.append(('HH'))
+        for i in xrange(0,m):
+                gene_pool.append(('Hh'))
+        for i in xrange(0,n):
+                gene_pool.append(('hh'))
+        gene_pool = tuple(gene_pool)
+        pairings = []
+        for x in itertools.combinations(gene_pool, 2):
+                pairings.append(x)
+        dominant_pairs, total_pairs = 0, 0
+        for mother, father in pairings:
+                inheiritance = (x+y for x in mother for y in father)
+                for gene_set in inheiritance:
+                        if 'H' in gene_set: dominant_pairs += 1
+                        total_pairs+=1
+        print float(dominant_pairs)/total_pairs
